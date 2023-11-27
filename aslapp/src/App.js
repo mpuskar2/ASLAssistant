f0c4f23660546436ca92b93928772a9b21c15b01
ethangingrich <egingric@uwo.ca>
2023-11-27 17:35:57 -0500

    First commit, created frontend

---
+++ aslapp/src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+import Settings from './component/Settings';
+import Camera from './component/Camera';
+import Text from './component/Text';
+
+function App() {
+  return (
+    <>
+    <Settings/>
+    <Camera/>
+    <Text/>
+    </>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  );
+}
+
+export default App;
---
+++ aslapp/src/component/Text.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Text() {
+    return (
+      <div id="titleDiv">
+         
+      </div>
+    )
+  }
+  
