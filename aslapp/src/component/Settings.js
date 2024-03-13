4eef06ea10e88c170de0120cbc54dc7b1679b746
ethangingrich <egingric@uwo.ca>
2024-03-12 20:34:37 -0400

    Added instructions for the user

--- aslapp/src/component/Settings.js
+++ aslapp/src/component/Settings.js
@@ -1,56 +1,104 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Settings() {
-  let tracker = false;
-    // function showSettings() {
-    //   let div = document.getElementById("settingsDiv");
+  const [isShown, setIsShown] = useState(false);
+  const [showAdditionalDiv, setShowAdditionalDiv] = useState(false);
 
-    //   if(!div.firstChildChild){
-    //     let br = document.createElement("br")
-    //     // setting1Button.innerText("setting 1 toggle");
-    //     let setting1Button = document.createElement("BUTTON");
-    //     setting1Button.onclick = function () {
-    //       showSettings();
-    //     }
-    //     div.append(br);
-    //     div.append(setting1Button);
-    //   } else {
-    //     while(div.hasChildNodes){
-    //       div.remove(div.firstChild);
-    //     }
-    //   }      
-    // }
-    function addButton() {
-      if (tracker == false){
-         var div = document.getElementById("settingsDiv");
-        var button = document.createElement("button");
-        button.innerHTML = "New Button";
-        div.appendChild(button);
-
-        tracker = true;
-      } else{
-       removeElements();
-      }
-
-    }
-
-    function removeElements() {
-      tracker = false;
-      var div = document.getElementById("settingsDiv");
-      while (div.firstChild) {
-        div.removeChild(div.firstChild);
-      }
-    }
-
-    return (
-      <div> 
-        <div>
-          <button onClick={() => addButton() }> show settings</button>
-          </div>
-        <div id="settingsDiv">
-
-        </div>
-      </div>
-    )
+  function toggleSettings() {
+    setIsShown(!isShown);
   }
-  
+
+  function toggleAdditionalDiv() {
+    setShowAdditionalDiv(!showAdditionalDiv);
+  }
+
+  function renderSettings() {
+    return (
+      <div>
+        Welcome to the ASL to assistant tool!
+        <br></br>
+        To start click the button below to show what letters you can sign into the camera and what commands they are linked to.
+        <br></br>
+        Once you start signing you will get a responce from the assistant which you can have read out to you via text to speach if you would like by enabling text to speach with the button bellow.
+        <br></br>
+        You can clear the chat history by clicking the clear chat button.
+        <br></br>
+        If you would prefer typing your comand you can use the text input feild and click send.
+        <br></br>
+        <button onClick={toggleAdditionalDiv}>Show letter to comand legend</button>
+      </div>
+    );
+  }
+
+  function renderAdditionalDiv() {
+    return (
+      <div>
+        Here is the legend:
+        <br></br>
+        a = 
+        <br></br>
+        b = 
+        <br></br>
+        c = 
+        <br></br>
+        d = 
+        <br></br>
+        e = 
+        <br></br>
+        f = 
+        <br></br>
+        g = 
+        <br></br>
+        h = Hi
+        <br></br>
+        i = 
+        <br></br>
+        j = 
+        <br></br>
+        l = 
+        <br></br>
+        m = 
+        <br></br>
+        n = 
+        <br></br>
+        o = 
+        <br></br>
+        p = 
+        <br></br>
+        q = 
+        <br></br>
+        r = 
+        <br></br>
+        s = 
+        <br></br>
+        t = test
+        <br></br>
+        u = 
+        <br></br>
+        v = 
+        <br></br>
+        w = weather
+        <br></br>
+        x = 
+        <br></br>
+        y = 
+        <br></br>
+        z = 
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <div>
+        <button onClick={toggleSettings}>Show Instructions</button>
+      </div>
+      {isShown && (
+        <div id="settingsDiv">
+          {renderSettings()}
+          {showAdditionalDiv && renderAdditionalDiv()}
+        </div>
+      )}
+    </div>
+  );
+}
